6b12789063fcfb24e49bbcc65fcb5f53adcc9f7b
valentina0720 <110473675+valentina0720@users.noreply.github.com>
2022-11-21 21:37:10 -0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -3,7 +3,7 @@ const PUBLISH_PRODUCT_URL = "https://japceibal.github.io/emercado-api/sell/publi
 const PRODUCTS_URL = "https://japceibal.github.io/emercado-api/cats_products/";
 const PRODUCT_INFO_URL = "https://japceibal.github.io/emercado-api/products/";
 const PRODUCT_INFO_COMMENTS_URL = "https://japceibal.github.io/emercado-api/products_comments/";
-const CART_INFO_URL = "https://japceibal.github.io/emercado-api/user_cart/";
+const CART_INFO_URL = "https://japceibal.github.io/emercado-api/user_cart/25801.json";
 const CART_BUY_URL = "https://japceibal.github.io/emercado-api/cart/buy.json";
 const EXT_TYPE = ".json";
 
